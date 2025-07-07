da17396814223c8b5515eb9d695a8eec7573c430
utkarshsharma19 <utkarshsharma124.us@gmail.com>
2025-07-07 05:30:59 -0700

    added labs details

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -7,6 +7,170 @@ export default function Lab1() {
         <h4>Heading Tags</h4>
 Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
       </div>
+      <p id="wd-p-2">
+This is the first paragraph. The paragraph tag is used to format
+vertical gaps between long pieces of text like this one.
+        </p>
+        <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        1. Mix dry ingredients.
+        2. Add wet ingredients.
+        3. Stir to combine.
+        4. Heat a skillet or griddle.
+        5. Pour batter onto the skillet.
+        6. Cook until bubbly on top.
+        7. Flip and cook the other side.
+        8. Serve and enjoy!
+      </div>
+      <div id="wd-lists">
+  <h4>List Tags</h4>
+  <h5>Ordered List Tag</h5>
+  How to make pancakes:
+  <ol id="wd-pancakes">
+    <li>Mix dry ingredients.</li>
+    <li>Add wet ingredients.</li>
+    <li>Stir to combine.</li>
+    <li>Heat a skillet or griddle.</li>
+    <li>Pour batter onto the skillet.</li>
+    <li>Cook until bubbly on top.</li>
+    <li>Flip and cook the other side.</li>
+    <li>Serve and enjoy!</li>
+  </ol>
+</div>
+My favorite recipe:
+<ol id="wd-my-favorite-recipe">{/* complete on your own */}</ol>
+<h5>Unordered List Tag</h5>
+My favorite books (in no particular order)
+<ul id="wd-my-books">
+  <li>Dune</li>
+  <li>Lord of the Rings</li>
+  <li>Ender's Game</li>
+  <li>Red Mars</li>
+  <li>The Forever War</li>
+</ul>
+Your favorite books (in no particular order)
+<ul id="wd-your-books">
+  {/* complete on your own */}
+</ul>
+<div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr> ... </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <h5>Text boxes</h5>
+<label>Biography:</label><br/>
+<textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+<h5 id="wd-buttons">Buttons</h5>
+<button type="button"
+        onClick={() => alert("Life is Good!")}
+        id="wd-all-good">
+  Hello World!
+</button>
+
+
+<h5 id="wd-radio-buttons">Radio buttons</h5>
+
+<label>Favorite movie genre:</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+<label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-drama"/>
+<label htmlFor="wd-radio-drama">Drama</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+<label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+<input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+<label htmlFor="wd-radio-fantasy">Fantasy</label>
+<h5 id="wd-checkboxes">Checkboxes</h5>
+<label>Favorite movie genre:</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+<label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+<label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+<label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+<label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+<h5>Select one</h5>
+<label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+<select id="wd-select-one-genre">
+   <option value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+
+
+<h4>Other HTML field types</h4>
+
+<label htmlFor="wd-text-fields-email"> Email: </label>
+<input type="email"
+       placeholder="jdoe@somewhere.com"
+       id="wd-text-fields-email"/><br/>
+
+<label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+<input type="number"
+       value="100000"
+       placeholder="1000"
+       id="wd-text-fields-salary-start"/><br/>
+
+<label htmlFor="wd-text-fields-rating"> Rating: </label>
+<input type="range"
+       value="4"
+       max="5"
+       placeholder="Doe"
+       id="wd-text-fields-rating"/><br/>
+
+<label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+
+<input type="date"
+       value="2000-01-21"
+       id="wd-text-fields-dob"/><br/>
+
+       
+        <h3>HTML Examples</h3>
+      <div id="wd-h-tag">
+        <h4>Heading Tags</h4>
+Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+      </div>
       </div>
   );}
   
