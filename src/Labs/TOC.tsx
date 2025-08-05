14fc96ce40cb4eb2a1a86309fe2fd4d19f6f96ed
utkarshsharma19 <utkarshsharma124.us@gmail.com>
2025-08-05 15:42:48 -0700

    github link added

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -17,6 +17,7 @@ export default function TOC() {
           active={pathname.includes("Lab5")}> Lab 5 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link href="https://github.com/utkarshsharma19/kambaz-react-web-app" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link href="https://github.com/utkarshsharma19/kambaz-node-server-app" target="_blank"> My Backend </Nav.Link> </Nav.Item>
     </Nav>
   );
 }
